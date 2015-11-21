e0e070cc19abc50a2a53d9afb479fdcbb7c49f79
zhou <zhou@zhoudemacbook-pro.local>
2015-11-22 01:27:01 +0800

    Initial Commit

---
+++ MidiPlayer_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MidiPlayer_Swift
+//
+//  Created by zhou on 15/11/22.
+//  Copyright © 2015年 zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
